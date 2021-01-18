a414537bf660b5486aa2e6bab3dfd1847c2720f3
Daniel <daniel.pereira.cunha@ua.pt>
2021-01-18 11:44:04 +0000

    stack

---
+++ stackClass.js
@@ -0,0 +1,30 @@
+class Stack{
+  constructor(){
+    this.collection = [];
+  }
+
+  push(e){
+    this.collection.push(e);
+  }
+
+  pop(){
+    return(this.collection.pop())
+  }
+
+  peek(){
+    return this.collection[this.collection.length-1]
+  }
+
+  isEmpty(){
+    return this.collection.length === 0 ? true : false;
+  }
+
+  clear(){
+    this.collection = [];
+  }
+ 
+  print(){
+    console.log(this.collection);
+  }
+}
+
